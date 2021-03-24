cb5daede586a752d578322aec046ac6d1682eab8
PetarMuzurovic <petarmuzurovic@gmail.com>
2021-03-24 16:23:55 +0100

    Konekcija sa mongo

--- Database/Database.js
+++ Database/Database.js
@@ -4,7 +4,18 @@ async function ConnectDatabase()
 {
     try 
     {
-        const link = "mongodb+srv://Hakerovi_drugari:Hakerovi_drugari@cluster0.6icaf.mongodb.net/Hakaton?retryWrites=true&w=majority";
+        // URL databaze
+        const link = 
+        "mongodb+srv://Hakerovi_drugari:Hakerovi_drugari@cluster0.6icaf.mongodb.net/Hakaton?retryWrites=true&w=majority";
+    
+        // konektovanje
+        const connection = await mongoose.connect(link, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true,
+        });
+
+        console.log("uspesno konektovanje");
     }
     catch (err) 
     {
